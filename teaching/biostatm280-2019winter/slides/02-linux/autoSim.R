4f0890be2ad927b434e22e2452c6403c79568490
Hua Zhou <huazhou@ucla.edu>
2019-01-15 17:51:33 +0000

    edits on teaching server

--- teaching/biostatm280-2019winter/slides/02-linux/autoSim.R
+++ teaching/biostatm280-2019winter/slides/02-linux/autoSim.R
@@ -1,10 +1,9 @@
 # autoSim.R
 
-nVals = seq(100, 500, by=100)
+nVals <- seq(100, 1000, by=100)
 for (n in nVals) {
-  oFile = paste("n", n, ".txt", sep="")
-  arg = paste("n=", n, sep="")
-  sysCall = paste("nohup Rscript runSim.R ", arg, " > ", oFile)
+  oFile <- paste("n", n, ".txt", sep="")
+  sysCall <- paste("nohup Rscript runSim.R n=", n, " > ", oFile, sep="")
   system(sysCall)
   print(paste("sysCall=", sysCall, sep=""))
 }
